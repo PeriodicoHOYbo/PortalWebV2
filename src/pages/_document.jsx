e230b5224f9c2b63babe69bbacf3f56ee8e44992
Your Name <you@example.com>
2024-09-05 03:18:32 -0400

    DC

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -11,13 +11,10 @@ export default class MyDocument extends Document {
                     <link rel="icon" href="h.png" />
                     <link rel='apple-touch-icon' href='/h.png' />
                     <link rel='manifest' href='/manifest.json' />
-                    <meta property="og:title" content={`Periodico Hoy Siete Dias De información`} />
-                    <meta property="og:description" content={`Las noticias mas actualizadas a nivel nacional e internacional lo tienes con HOY.BO.`} />
-                    <meta property="og:image" content='/logo.png' />
                     <meta name="theme-color" content="#007483" />
                     <meta name="msapplication-navbutton-color" content="#007483" />
                     <meta name="apple-mobile-web-app-status-bar-style" content="#007483" />
-                    <meta name="description" content="Periódico HOY Bolivia" />
+                    <meta name="description" content="Periodico Hoy Siete Dias De información" />
                     <meta name="keywords" content="Hoy, hoy bo,  hoy.bo, Periódico HOY, Periódico HOY Bolivia, hoy bolivia, periodico bolivia,
                      noticias hoy, hoy noticias, noticias bolivia, peridico noticias, peridico, noticias,
                      la razon, el deber, pagina siete, diario pagina siete, hoybolivia.com, diario bolivia" />
