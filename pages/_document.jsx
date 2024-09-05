ae21b6dcabf8aad3606791c0a1ef69f10a394f57
Your Name <you@example.com>
2024-09-05 00:23:35 -0400

    DC

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -14,13 +14,13 @@ export default class MyDocument extends Document {
                     <meta name="theme-color" content="#007483" />
                     <meta name="msapplication-navbutton-color" content="#007483" />
                     <meta name="apple-mobile-web-app-status-bar-style" content="#007483" />
-{/*                     <meta name="description" content="Periódico HOY Bolivia" />
+                    <meta name="description" content="Periódico HOY Bolivia" />
                     <meta name="keywords" content="Hoy, hoy bo,  hoy.bo, Periódico HOY, Periódico HOY Bolivia, hoy bolivia, periodico bolivia,
                      noticias hoy, hoy noticias, noticias bolivia, peridico noticias, peridico, noticias,
                      la razon, el deber, pagina siete, diario pagina siete, hoybolivia.com, diario bolivia" />
                     <meta name="author" content="Hoy" />
                     <title>Hoy Bolivia</title>
-                    <meta name="google-adsense-account" content="ca-pub-9268005466612059" /> */}
+                    <meta name="google-adsense-account" content="ca-pub-9268005466612059" />
                 </Head> 
 
                 <body>
